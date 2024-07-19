1cf864239dd4178e9cce828a50a094f0fc9e4b8b
Kayane  Alebrante <107808062+KayaneAlebrante@users.noreply.github.com>
2024-07-19 18:45:18 -0300

    Feat UseState

--- 1_Projeto/MyFinances/src/App.tsx
+++ 1_Projeto/MyFinances/src/App.tsx
@@ -1,12 +1,45 @@
+import { useState } from 'react'
 import './App.css'
 import FinanceControl from './components/FinanceControl/FinaceControl'
 import Header from './components/Header/Header'
+import { Movement } from './models/interfaces/Movement/Movement';
 
 function App() {
-  const finances = () => {
-  alert("Acionado")
-  }
+ const [currentBalance, setCurrentBalance] = useState(0); //Saldo Atual
+ const [currentExpenses, setCurrentExpenses] = useState(0); //Despesaas Atual
+ const [movementsItens, setMovementsIntes] = useState<Array<Movement>>([]); //Movimentações
 
+
+ const setNewMovement = (movement: Movement) => {
+  if(movement){
+    setMovementsIntes((prevMovements) => {
+      const movements = {...prevMovements};
+      movements.unshift({
+        name: movement.name,
+        value: movement.value,
+        type: movement.type,
+        id: Math.random().toString(),
+      });
+      return movements;
+    });
+
+    movement.type === "Input" && setCurrentBalance(
+      (prevBalance) => prevBalance + Number(movement.value)
+    );
+
+    if(movement.type === "Output"){
+      setCurrentExpenses(
+        (prevBalance) => prevBalance + Number(movement.value)
+      );
+    }
+
+    currentBalance > 0 && setCurrentBalance(
+      (prevBalance) => prevBalance - Number(movement.value)
+    );
+  }
+ };
+
+ 
   return (
     <div>
       <Header />
@@ -15,4 +48,5 @@ function App() {
   )
 }
 
-export default App
+export default App;
+ 
